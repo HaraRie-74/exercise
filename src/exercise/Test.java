c7e5300452bc2006ef47cd5d0b729574f1d132d7
hnyrk <hnyrk@192.168.0.24>
2023-06-02 20:46:34 +0900

    switch文　練習

--- src/exercise/Test.java
+++ src/exercise/Test.java
@@ -3,16 +3,21 @@ package exercise;
 public class Test {
 
 	public static void main(String[] args) {
-		int seibetsu = 1;
-		int age = 22;
-		System.out.println("こんにちは");
-		if (seibetsu == 0) {
-			System.out.println("私は男です。");
-			System.out.println(age + "歳です。");
-		} else {
-			System.out.println("私は女です。");
+		System.out.println("[メニュー]1:検索 2:登録 3:削除 4:変更 ＞");
+		int selected = new java.util.Scanner(System.in).nextInt();
+		switch (selected) {
+		    case 1:
+		    	System.out.println("検索します");
+		    	break;
+		    case 2:
+		    	System.out.println("登録します");
+		    	break;
+		    case 3:
+		    	System.out.println("削除します");
+		    	break;
+		    case 4:
+		    	System.out.println("変更します");
+		    	break;
 		}
-		System.out.println("宜しくお願いいたします。");
 	}
-
 }
