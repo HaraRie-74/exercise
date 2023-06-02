5b09b8f7ae6f96e3297bcf171489a7d6a1911b3a
hnyrk <hnyrk@192.168.0.24>
2023-06-02 20:13:35 +0900

    for分　九九作成

---
+++ src/exercise/Test.java
@@ -0,0 +1,15 @@
+package exercise;
+
+public class Test {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 10; i++) {
+			for (int y = 1; y <10; y++) {
+				System.out.print(i * y);
+			    System.out.print(" ");
+			}
+		System.out.println("");
+		}
+	}
+
+}
