52d13afe736a48abd1397bf00631f81f674b8075
HaraRie-74 <hnyrk1telolalb@gmail.com>
2023-06-03 21:23:08 +0900

    メソッド学習

--- src/exercise/Test.java
+++ src/exercise/Test.java
@@ -1,15 +1,14 @@
 package exercise;
 
 public class Test {
-
+	
 	public static void main(String[] args) {
-		int [] numbers = {3, 4, 9};
-		System.out.println("１桁の数字を入力してください");
-		int input = new java.util.Scanner(System.in).nextInt();
-		for (int value : numbers) {
-			if (value == input) {
-				System.out.println("アタリ！！！！！");
-			}
-		}
+		fruit("りんご", 5);
+		fruit("梨", 8);
 	}
+	
+	public static void fruit (String name, int piece) {
+		System.out.println(name + "を" + piece + "個買いました。");
+	}
+	
 }
