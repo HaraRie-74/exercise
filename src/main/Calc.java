2d699dffb6cf755e8f910cef0a26ed262089327f
HaraRie-74 <hnyrk1telolalb@gmail.com>
2023-06-04 19:38:11 +0900

    複数パッケージ　import使用

--- src/main/Calc.java
+++ src/main/Calc.java
@@ -1,12 +1,18 @@
 package main;
+import logics.CalcLogic;
+//このソースコードで CalcLogic という表記が合ったら、それはlogics.CalcLogicのことだと解釈しなさいという指示
+
+//import logics.CalcLogic;
+//logicsパッケージに所属する全てのクラスをインポートしたいとき
 
 public class Calc {
 
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 5;
-		int total = logics.CalcLogic.tasu(a, b);
-		int dalta = logics.CalcLogic.hiku(a, b);
+		int total = CalcLogic.tasu(a, b);
+		int dalta = CalcLogic.hiku(a, b);
+//		importでパッケージを読み込んでいるので、クラス.メソッドのみでOK
 		System.out.println("足すと" + total + "、引くと" + dalta);
 	}
 
